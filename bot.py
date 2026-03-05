cc6661ae5e5fe5eefb1a2ab0a3bf543b12596ee1
nimben <nimbenx20@gmail.com>
2026-03-05 12:06:06 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,9 @@
+import requests
+def check_price():
+    print("Checking price...")
+    price=2500
+    rating=4.5
+    print(f"Price: {price}, Rating: {rating}")
+    
+    if __name__ == "__main__":
+        check_price()
